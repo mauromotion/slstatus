5c1f75851ae617c91761a797d62e22a59ef7626c
mauromotion <hello@mauromotion.com>
2024-06-18 18:49:03 +0100

    fixed updates script

--- config.h
+++ config.h
@@ -65,6 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
+  { run_command, "%s", "~/.dotfiles/desktop/polybar/.config/polybar/polybar-scripts/updates-pacman-aurhelper.sh" },
   { cpu_perc, " CPU: %s%% | ",                   NULL },
   { ram_perc, "RAM: %s%% | ",                   NULL },
   { disk_perc, "HD1: %s%% | ",                    "/" },
