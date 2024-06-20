7a25fddc4bca311754d6b727907883f12fd43386
mauromotion <hello@mauromotion.com>
2024-06-20 11:58:36 +0100

    added scripts dir, added checkupdates.sh with cache system for specific check interval

---
+++ scripts/check_updates.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+CACHE_FILE="/tmp/check_updates.cache"
+CACHE_INTERVAL=300 # 5 minutes
+
+# Check if the cache file exists and is not older than the interval
+if [ -f "$CACHE_FILE" ]; then
+	if [ "$(($(date +%s) - $(date +%s -r "$CACHE_FILE")))" -lt "$CACHE_INTERVAL" ]; then
+		cat "$CACHE_FILE"
+		exit 0
+	fi
+fi
+
+if ! updates_arch=$(checkupdates 2>/dev/null | wc -l); then
+	updates_arch=0
+fi
+
+if ! updates_aur=$(yay -Qum 2>/dev/null | wc -l); then
+	updates_aur=0
+fi
+
+updates=$((updates_arch + updates_aur))
+
+if [ "$updates" -gt 0 ]; then
+	echo " $updates | " >"$CACHE_FILE"
+	echo " $updates | "
+else
+	echo "​" >"$CACHE_FILE"
+	echo "​"
+fi
